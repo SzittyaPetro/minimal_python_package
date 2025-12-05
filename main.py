7643ae1e640a3b80b5811ea6f73eb68ff2b56db6
Nyilas Péter <nyilaspeter001@gmail.com>
2025-12-05 07:56:54 +0100

    bad commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from minimal-python-package!")
-
+def  main( ):
+ print('Hello') # bad indentation, single quotes, whitespace
+  x = 1
+  y=2
+  print(x+y)
 
 if __name__ == "__main__":
     main()
